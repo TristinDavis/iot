5afa860bcfce533ce021c0d56a63871dbfeb2b62
Vasiliy Sadokhin <vasiliy.sadokhin@gmail.com>
2018-09-26 23:39:04 +0800

    Removed extra port binding

--- stream-consumer/run.sh
+++ stream-consumer/run.sh
@@ -2,7 +2,6 @@
 ../gradlew clean bootJar
 docker rm -f iot-stream-consumer
 docker run --rm -d --name iot-stream-consumer \
--p 8081:8080 \
 --link iot-kafka:iot-kafka \
 --link iot-cassandra:iot-cassandra \
 -v `readlink -f build/libs/stream-consumer-0.0.1-SNAPSHOT.jar`:/tmp/stream-consumer.jar \
