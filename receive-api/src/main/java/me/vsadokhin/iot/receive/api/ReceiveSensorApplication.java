cbeca3e38d6670cc4ff789c984c41c14eebd6c35
Vasiliy Sadokhin <vasiliy.sadokhin@gmail.com>
2018-09-27 16:36:24 +0800

    Made receive api module not to init cassandra

--- receive-api/src/main/java/me/vsadokhin/iot/receive/api/ReceiveSensorApplication.java
+++ receive-api/src/main/java/me/vsadokhin/iot/receive/api/ReceiveSensorApplication.java
@@ -2,8 +2,9 @@ package me.vsadokhin.iot.receive.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = CassandraDataAutoConfiguration.class)
 public class ReceiveSensorApplication {
 
     public static void main(String... args) {
