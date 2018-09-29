50d245c7472770b60ab88c5fa872fb638075b016
Vasiliy Sadokhin <vasiliy.sadokhin@gmail.com>
2018-09-29 20:48:45 +0800

    Added basic authorization based on in memory user for test purpose

--- statistics-api/src/main/java/me/vsadokhin/iot/statistics/api/StatisticsConfig.java
+++ statistics-api/src/main/java/me/vsadokhin/iot/statistics/api/StatisticsConfig.java
@@ -1,11 +1,37 @@
 package me.vsadokhin.iot.statistics.api;
 
 import me.vsadokhin.iot.data.MetricRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
-public class StatisticsConfig {
+@EnableWebSecurity
+public class StatisticsConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("getStatisticsUser")
+                .password("statistics123").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/securityNone").permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic().realmName("Test Realm")
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
 
     @Bean
     public MetricRepository metricRepository() {
