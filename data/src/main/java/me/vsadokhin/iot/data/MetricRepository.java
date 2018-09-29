3fb7010b00c36a0432162532f07b8bdca06c1430
Vasiliy Sadokhin <vasiliy.sadokhin@gmail.com>
2018-09-29 16:17:54 +0800

    Removed test code

--- data/src/main/java/me/vsadokhin/iot/data/MetricRepository.java
+++ data/src/main/java/me/vsadokhin/iot/data/MetricRepository.java
@@ -7,10 +7,6 @@ import static com.datastax.driver.core.querybuilder.QueryBuilder.in;
 import static com.datastax.driver.core.querybuilder.QueryBuilder.lt;
 import static me.vsadokhin.iot.data.MetricTable.METRIC_BY_SENSOR;
 
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
@@ -36,10 +32,6 @@ public class MetricRepository {
         insert.value("sensor_id", metric.getSensorId());
         insert.value("when", metric.getWhen());
         insert.value("value", metric.getValue());
-        ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(Instant.ofEpochMilli(metric.getWhen()), ZoneId.of("UTC"));
-        zonedDateTime.getYear();
-        zonedDateTime.getMonth();
-        zonedDateTime.getDayOfMonth();
         
         if (METRIC_BY_SENSOR.equals(table)) {
             insert.value("week", DateUtility.getWeekStart(metric.getWhen()));
