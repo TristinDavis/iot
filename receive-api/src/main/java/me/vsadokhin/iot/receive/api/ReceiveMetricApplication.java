fc13b7890e2c7c92a8e0a7218f34d2e6fb21019a
Vasiliy Sadokhin <vasiliy.sadokhin@gmail.com>
2018-09-30 21:57:57 +0800

    Added scanBasePackages to fix API after moving KafkaProducerConfig

--- receive-api/src/main/java/me/vsadokhin/iot/receive/api/ReceiveMetricApplication.java
+++ receive-api/src/main/java/me/vsadokhin/iot/receive/api/ReceiveMetricApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration;
 
-@SpringBootApplication(exclude = CassandraDataAutoConfiguration.class)
+@SpringBootApplication(exclude = CassandraDataAutoConfiguration.class,
+        scanBasePackages = { "me.vsadokhin.iot.receive.api", "me.vsadokhin.iot.stream.producer" })
 public class ReceiveMetricApplication {
 
     public static void main(String... args) {
-   		SpringApplication.run(ReceiveMetricApplication.class, args);
-   	}
-   	
+        SpringApplication.run(ReceiveMetricApplication.class, args);
+    }
+
 }
