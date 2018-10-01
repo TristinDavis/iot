1e8ce52092b6a5a503347e27fbc2aaeb05d872d5
Vasiliy Sadokhin <vasiliy.sadokhin@gmail.com>
2018-10-01 21:04:12 +0800

    Added tests for receive api to check corner cases

--- qa/receive-api/src/test/java/me/vsadokhin/iot/qa/receive/api/ReceiveMetricTest.java
+++ qa/receive-api/src/test/java/me/vsadokhin/iot/qa/receive/api/ReceiveMetricTest.java
@@ -21,6 +21,7 @@ import me.vsadokhin.iot.data.utility.CassandraClusterUtility;
 import me.vsadokhin.iot.data.utility.CassandraSessionUtility;
 import me.vsadokhin.iot.data.utility.DateUtility;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.http.HttpEntity;
@@ -31,6 +32,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 public class ReceiveMetricTest {
@@ -38,7 +40,8 @@ public class ReceiveMetricTest {
     private static RestTemplate REST_TEMPLATE = new RestTemplate();
 
     private static Session SESSION = CassandraSessionUtility.getSession();
-    public static final HttpHeaders HEADERS = new HttpHeaders();
+
+    private static final HttpHeaders HEADERS = new HttpHeaders();
 
     @BeforeClass
     public static void setUpClass() {
@@ -49,10 +52,6 @@ public class ReceiveMetricTest {
 
         REST_TEMPLATE.setMessageConverters(messageConverters);
         HEADERS.setContentType(MediaType.APPLICATION_JSON);
-
-        for (MetricTable metricTable : MetricTable.values()) {
-            SESSION.execute(QueryBuilder.truncate(metricTable.getTable()));
-        }
     }
 
     @AfterClass
@@ -60,6 +59,12 @@ public class ReceiveMetricTest {
         CassandraClusterUtility.closeCluster();
     }
 
+    @Before
+    public void setUp() {
+        for (MetricTable metricTable : MetricTable.values()) {
+            SESSION.execute(QueryBuilder.truncate(metricTable.getTable()));
+        }
+    }
 
     private Row waitMetricInDB(Select select) {
         long startTime = System.currentTimeMillis();
@@ -74,6 +79,21 @@ public class ReceiveMetricTest {
             }
         }
         throw new AssertionError("Metric is not found, finished by timeout");
+    }
+
+    private Select prepareSelectFromMetricBySensorId(Metric metric) {
+        Select select;
+        select = QueryBuilder.select().from(MetricTable.METRIC_BY_SENSOR.getTable());
+        select.where(QueryBuilder.eq("sensor_id", metric.getSensorId()))
+                .and(QueryBuilder.eq("week", DateUtility.getWeekStart(metric.getWhen())));
+        return select;
+    }
+
+    private Select prepareSelectFromMetricByType(Metric metric) {
+        Select select = QueryBuilder.select().from(MetricTable.METRIC_BY_TYPE.getTable());
+        select.where(QueryBuilder.eq("type", metric.getType()))
+                .and(QueryBuilder.eq("day", DateUtility.getDayStart(metric.getWhen())));
+        return select;
     }
 
     @Test
@@ -91,21 +111,92 @@ public class ReceiveMetricTest {
         assertThat(result.getStatusCode(), is(HttpStatus.ACCEPTED));
         assertThat(result.getBody(), is(nullValue()));
 
-        Select select = QueryBuilder.select().from(MetricTable.METRIC_BY_TYPE.getTable());
-        select.where(QueryBuilder.eq("type", metric.getType()))
-                .and(QueryBuilder.eq("day", DateUtility.getDayStart(metric.getWhen())));
+        Select select = prepareSelectFromMetricByType(metric);
 
         Row row = waitMetricInDB(select);
         assertThat(row.getString("sensor_id"), is(metric.getSensorId()));
         assertThat(row.getTimestamp("when").getTime(), is(metric.getWhen()));
         assertThat(row.getFloat("value"), is(metric.getValue()));
 
-        select = QueryBuilder.select().from(MetricTable.METRIC_BY_SENSOR.getTable());
-        select.where(QueryBuilder.eq("sensor_id", metric.getSensorId()))
-                .and(QueryBuilder.eq("week", DateUtility.getWeekStart(metric.getWhen())));
+        select = prepareSelectFromMetricBySensorId(metric);
         row = waitMetricInDB(select);
         assertThat(row.getTimestamp("when").getTime(), is(metric.getWhen()));
         assertThat(row.getFloat("value"), is(metric.getValue()));
     }
-    
+
+    private void waitNoMetricInDB(Metric metric) {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ResultSet resultSet;
+        if (metric.getSensorId() != null) {
+            resultSet = SESSION.execute(prepareSelectFromMetricBySensorId(metric));
+            assertThat(resultSet.getAvailableWithoutFetching(), is(0));
+        }
+
+        if (metric.getType() != null) {
+            resultSet = SESSION.execute(prepareSelectFromMetricByType(metric));
+            assertThat(resultSet.getAvailableWithoutFetching(), is(0));
+        }
+    }
+
+    private void sendMetric_requiredFieldIsEmpty_checkResponseIsBadRequest_checkNoMetricInDB(Metric metric) {
+        HttpEntity<Metric> entity = new HttpEntity<>(metric, HEADERS);
+        try {
+            // act
+            REST_TEMPLATE.exchange("http://localhost:8080/metric", HttpMethod.POST, entity, ResponseEntity.class);
+            // verify
+            fail();
+        } catch (HttpClientErrorException e) {
+            assertThat(e.getRawStatusCode(), is(HttpStatus.BAD_REQUEST.value()));
+        }
+        waitNoMetricInDB(metric);
+    }
+
+    @Test
+    public void sendMetric_noType_checkResponseIsBadRequest_checkNoMetricInDB() {
+        // setup
+        Metric metric = new MetricBuilder("sensor2", null)
+                .setValue(1.23F)
+                .setWhen(System.currentTimeMillis()).build();
+
+        // act & verify
+        sendMetric_requiredFieldIsEmpty_checkResponseIsBadRequest_checkNoMetricInDB(metric);
+    }
+
+    @Test
+    public void sendMetric_noSensorId_checkResponse() {
+        // setup
+        Metric metric = new MetricBuilder(null, "type2")
+                .setValue(1.23F)
+                .setWhen(System.currentTimeMillis()).build();
+
+        // act & verify
+        sendMetric_requiredFieldIsEmpty_checkResponseIsBadRequest_checkNoMetricInDB(metric);
+    }
+
+    @Test
+    public void sendMetric_noValue_checkResponse() {
+        // setup
+        Metric metric = new MetricBuilder("sensor3", "type3")
+                .setValue(null)
+                .setWhen(System.currentTimeMillis()).build();
+
+        // act & verify
+        sendMetric_requiredFieldIsEmpty_checkResponseIsBadRequest_checkNoMetricInDB(metric);
+    }
+
+    @Test
+    public void sendMetric_noWhen_checkResponse() {
+        // setup
+        Metric metric = new MetricBuilder("sensor4", "type4")
+                .setValue(null)
+                .setWhen(System.currentTimeMillis()).build();
+
+        // act & verify
+        sendMetric_requiredFieldIsEmpty_checkResponseIsBadRequest_checkNoMetricInDB(metric);
+    }
+
 }
