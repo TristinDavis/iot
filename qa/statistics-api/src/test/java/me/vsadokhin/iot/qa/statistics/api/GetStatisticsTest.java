bbf7b7ebbfb8f4c7e280d2d19fe54675a8137116
Vasiliy Sadokhin <vasiliy.sadokhin@gmail.com>
2018-10-01 21:49:25 +0800

    Added tests for get statistics api to check corner cases

--- qa/statistics-api/src/test/java/me/vsadokhin/iot/qa/statistics/api/GetStatisticsTest.java
+++ qa/statistics-api/src/test/java/me/vsadokhin/iot/qa/statistics/api/GetStatisticsTest.java
@@ -2,7 +2,10 @@ package me.vsadokhin.iot.qa.statistics.api;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.fail;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -22,17 +25,19 @@ import org.junit.Test;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 public class GetStatisticsTest {
 
     private static final String ENDPOINT = "http://localhost:8081/statistics";
-    
+
     private static RestTemplate REST_TEMPLATE = new RestTemplate();
 
     private static Session SESSION = CassandraSessionUtility.getSession();
@@ -40,7 +45,7 @@ public class GetStatisticsTest {
     private static final HttpHeaders HEADERS = new HttpHeaders();
 
     private static final MetricRepository METRIC_REPOSITORY = new MetricRepository();
-    
+
     private HttpEntity<Metric> entity;
 
     @BeforeClass
@@ -76,19 +81,19 @@ public class GetStatisticsTest {
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_SENSOR);
 
         metric = new MetricBuilder("sensor1", "type1")
-                        .setValue(1.0F).setWhen(1L).build();
+                .setValue(1.0F).setWhen(1L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_SENSOR);
 
         metric = new MetricBuilder("sensor2", "type1")
-                        .setValue(Float.MIN_VALUE).setWhen(1L).build();
+                .setValue(Float.MIN_VALUE).setWhen(1L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_SENSOR);
 
         metric = new MetricBuilder("sensor1", "type1")
-                        .setValue(2.0F).setWhen(2L).build();
+                .setValue(2.0F).setWhen(2L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_SENSOR);
 
         metric = new MetricBuilder("sensor1", "type1")
-                        .setValue(Float.MIN_VALUE).setWhen(3L).build();
+                .setValue(Float.MIN_VALUE).setWhen(3L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_SENSOR);
 
         // act
@@ -101,6 +106,7 @@ public class GetStatisticsTest {
                 HttpMethod.GET, entity, Float.class);
 
         // verify
+        assertThat(result.getStatusCodeValue(), is(HttpStatus.OK.value()));
         assertThat(result.getBody(), is(1.0F));
     }
 
@@ -112,19 +118,19 @@ public class GetStatisticsTest {
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_SENSOR);
 
         metric = new MetricBuilder("sensor1", "type1")
-                        .setValue(1.0F).setWhen(1L).build();
+                .setValue(1.0F).setWhen(1L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_SENSOR);
 
         metric = new MetricBuilder("sensor2", "type1")
-                        .setValue(Float.MAX_VALUE).setWhen(1L).build();
+                .setValue(Float.MAX_VALUE).setWhen(1L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_SENSOR);
 
         metric = new MetricBuilder("sensor1", "type1")
-                        .setValue(2.0F).setWhen(2L).build();
+                .setValue(2.0F).setWhen(2L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_SENSOR);
 
         metric = new MetricBuilder("sensor1", "type1")
-                        .setValue(Float.MAX_VALUE).setWhen(3L).build();
+                .setValue(Float.MAX_VALUE).setWhen(3L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_SENSOR);
 
         // act
@@ -137,6 +143,7 @@ public class GetStatisticsTest {
                 HttpMethod.GET, entity, Float.class);
 
         // verify
+        assertThat(result.getStatusCodeValue(), is(HttpStatus.OK.value()));
         assertThat(result.getBody(), is(2.0F));
     }
 
@@ -148,19 +155,19 @@ public class GetStatisticsTest {
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_SENSOR);
 
         metric = new MetricBuilder("sensor1", "type1")
-                        .setValue(1.0F).setWhen(1L).build();
+                .setValue(1.0F).setWhen(1L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_SENSOR);
 
         metric = new MetricBuilder("sensor2", "type1")
-                        .setValue(123F).setWhen(1L).build();
+                .setValue(123F).setWhen(1L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_SENSOR);
 
         metric = new MetricBuilder("sensor1", "type1")
-                        .setValue(2.0F).setWhen(2L).build();
+                .setValue(2.0F).setWhen(2L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_SENSOR);
 
         metric = new MetricBuilder("sensor1", "type1")
-                        .setValue(123F).setWhen(3L).build();
+                .setValue(123F).setWhen(3L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_SENSOR);
 
         // act
@@ -173,6 +180,7 @@ public class GetStatisticsTest {
                 HttpMethod.GET, entity, Float.class);
 
         // verify
+        assertThat(result.getStatusCodeValue(), is(HttpStatus.OK.value()));
         assertThat(result.getBody(), is(1.5F));
     }
 
@@ -184,19 +192,19 @@ public class GetStatisticsTest {
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_TYPE);
 
         metric = new MetricBuilder("sensor2", "type1")
-                        .setValue(1.0F).setWhen(1L).build();
+                .setValue(1.0F).setWhen(1L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_TYPE);
 
         metric = new MetricBuilder("sensor3", "type2")
-                        .setValue(Float.MIN_VALUE).setWhen(1L).build();
+                .setValue(Float.MIN_VALUE).setWhen(1L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_TYPE);
 
         metric = new MetricBuilder("sensor1", "type1")
-                        .setValue(2.0F).setWhen(2L).build();
+                .setValue(2.0F).setWhen(2L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_TYPE);
 
         metric = new MetricBuilder("sensor1", "type1")
-                        .setValue(Float.MIN_VALUE).setWhen(3L).build();
+                .setValue(Float.MIN_VALUE).setWhen(3L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_TYPE);
 
         // act
@@ -209,6 +217,7 @@ public class GetStatisticsTest {
                 HttpMethod.GET, entity, Float.class);
 
         // verify
+        assertThat(result.getStatusCodeValue(), is(HttpStatus.OK.value()));
         assertThat(result.getBody(), is(1.0F));
     }
 
@@ -220,19 +229,19 @@ public class GetStatisticsTest {
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_TYPE);
 
         metric = new MetricBuilder("sensor2", "type1")
-                        .setValue(1.0F).setWhen(1L).build();
+                .setValue(1.0F).setWhen(1L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_TYPE);
 
         metric = new MetricBuilder("sensor3", "type2")
-                        .setValue(Float.MAX_VALUE).setWhen(1L).build();
+                .setValue(Float.MAX_VALUE).setWhen(1L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_TYPE);
 
         metric = new MetricBuilder("sensor1", "type1")
-                        .setValue(2.0F).setWhen(2L).build();
+                .setValue(2.0F).setWhen(2L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_TYPE);
 
         metric = new MetricBuilder("sensor1", "type1")
-                        .setValue(Float.MAX_VALUE).setWhen(3L).build();
+                .setValue(Float.MAX_VALUE).setWhen(3L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_TYPE);
 
         // act
@@ -245,6 +254,7 @@ public class GetStatisticsTest {
                 HttpMethod.GET, entity, Float.class);
 
         // verify
+        assertThat(result.getStatusCodeValue(), is(HttpStatus.OK.value()));
         assertThat(result.getBody(), is(2.0F));
     }
 
@@ -256,19 +266,19 @@ public class GetStatisticsTest {
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_TYPE);
 
         metric = new MetricBuilder("sensor2", "type1")
-                        .setValue(1.0F).setWhen(1L).build();
+                .setValue(1.0F).setWhen(1L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_TYPE);
 
         metric = new MetricBuilder("sensor3", "type2")
-                        .setValue(123F).setWhen(1L).build();
+                .setValue(123F).setWhen(1L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_TYPE);
 
         metric = new MetricBuilder("sensor1", "type1")
-                        .setValue(2.0F).setWhen(2L).build();
+                .setValue(2.0F).setWhen(2L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_TYPE);
 
         metric = new MetricBuilder("sensor1", "type1")
-                        .setValue(123F).setWhen(3L).build();
+                .setValue(123F).setWhen(3L).build();
         METRIC_REPOSITORY.insert(metric, MetricTable.METRIC_BY_TYPE);
 
         // act
@@ -281,7 +291,121 @@ public class GetStatisticsTest {
                 HttpMethod.GET, entity, Float.class);
 
         // verify
+        assertThat(result.getStatusCodeValue(), is(HttpStatus.OK.value()));
         assertThat(result.getBody(), is(1.5F));
     }
+
+    @Test
+    public void getStatistics_noType_noSesnorId_checkError() {
+        try {
+            // act
+            UriComponentsBuilder uirBuilder = UriComponentsBuilder.fromHttpUrl(ENDPOINT)
+                    .queryParam("aggregator", "avg")
+                    .queryParam("from", 1)
+                    .queryParam("to", 2);
+            REST_TEMPLATE.exchange(uirBuilder.toUriString(), HttpMethod.GET, entity, String.class);
+            
+            // verify
+            fail();
+        } catch (HttpClientErrorException e) {
+            assertThat(e.getStatusCode(), is(HttpStatus.BAD_REQUEST));
+            assertThat(e.getResponseBodyAsString(), is("Type or sensorId must be specified"));
+        }
+    }
+
+    @Test
+    public void getStatistics_typeAndSensorIdBothAreSpecified_checkError() {
+        try {
+            // act
+            UriComponentsBuilder uirBuilder = UriComponentsBuilder.fromHttpUrl(ENDPOINT)
+                    .queryParam("sensorId", "sensorId")
+                    .queryParam("type", "type")
+                    .queryParam("aggregator", "avg")
+                    .queryParam("from", 1)
+                    .queryParam("to", 2);
+            REST_TEMPLATE.exchange(uirBuilder.toUriString(), HttpMethod.GET, entity, String.class);
+
+            // verify
+            fail();
+        } catch (HttpClientErrorException e) {
+            assertThat(e.getStatusCode(), is(HttpStatus.BAD_REQUEST));
+            assertThat(e.getResponseBodyAsString(), is("Only type or only sensorId must be specified"));
+        }
+    }
+
+    @Test
+    public void getStatistics_unsupportedAggregator_checkError() {
+        try {
+            // act
+            UriComponentsBuilder uirBuilder = UriComponentsBuilder.fromHttpUrl(ENDPOINT)
+                    .queryParam("sensorId", "sensorId")
+                    .queryParam("aggregator", "ABCDE")
+                    .queryParam("from", 1)
+                    .queryParam("to", 2);
+            REST_TEMPLATE.exchange(uirBuilder.toUriString(), HttpMethod.GET, entity, String.class);
+
+            // verify
+            fail();
+        } catch (HttpClientErrorException e) {
+            assertThat(e.getStatusCode(), is(HttpStatus.BAD_REQUEST));
+            assertThat(e.getResponseBodyAsString(), is("ABCDE is not supported aggregator. Supported values: min, max, avg"));
+        }
+    }
+
+    @Test
+    public void getStatistics_toIsBeforeFrom_checkError() {
+        try {
+            // act
+            UriComponentsBuilder uirBuilder = UriComponentsBuilder.fromHttpUrl(ENDPOINT)
+                    .queryParam("sensorId", "sensorId")
+                    .queryParam("aggregator", "avg")
+                    .queryParam("from", 2)
+                    .queryParam("to", 1);
+            REST_TEMPLATE.exchange(uirBuilder.toUriString(), HttpMethod.GET, entity, String.class);
+
+            // verify
+            fail();
+        } catch (HttpClientErrorException e) {
+            assertThat(e.getStatusCode(), is(HttpStatus.BAD_REQUEST));
+            assertThat(e.getResponseBodyAsString(), is("To must be greater than From"));
+        }
+    }
     
+    @Test
+    public void getStatistics_bySensor_fromAndToBelongToDifferentWeeks_checkError() {
+        try {
+            // act
+            UriComponentsBuilder uirBuilder = UriComponentsBuilder.fromHttpUrl(ENDPOINT)
+                    .queryParam("sensorId", "sensorId")
+                    .queryParam("aggregator", "avg")
+                    .queryParam("from", LocalDateTime.now().minusDays(8).toInstant(ZoneOffset.UTC).toEpochMilli())
+                    .queryParam("to", System.currentTimeMillis());
+            REST_TEMPLATE.exchange(uirBuilder.toUriString(), HttpMethod.GET, entity, String.class);
+
+            // verify
+            fail();
+        } catch (HttpClientErrorException e) {
+            assertThat(e.getStatusCode(), is(HttpStatus.BAD_REQUEST));
+            assertThat(e.getResponseBodyAsString(), is("From and To must point to the same week for aggregate by sensor query"));
+        }
+    }
+
+    @Test
+    public void getStatistics_byType_fromAndToBelongToDifferentDays_checkError() {
+        try {
+            // act
+            UriComponentsBuilder uirBuilder = UriComponentsBuilder.fromHttpUrl(ENDPOINT)
+                    .queryParam("type", "type")
+                    .queryParam("aggregator", "avg")
+                    .queryParam("from", LocalDateTime.now().minusDays(1).toInstant(ZoneOffset.UTC).toEpochMilli())
+                    .queryParam("to", System.currentTimeMillis());
+            REST_TEMPLATE.exchange(uirBuilder.toUriString(), HttpMethod.GET, entity, String.class);
+
+            // verify
+            fail();
+        } catch (HttpClientErrorException e) {
+            assertThat(e.getStatusCode(), is(HttpStatus.BAD_REQUEST));
+            assertThat(e.getResponseBodyAsString(), is("From and To must point to the same day for aggregate by type query"));
+        }
+    }
 }
